b863934794d12eb9bbc19d299c3be48273340cb0
Jackie Chen <ecuojackie@yahoo.com>
2022-06-26 23:12:03 -0700

    working dictionary app

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,18 +1,32 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import tw from "twrnc"
+import axios from 'axios'
+import { useEffect } from 'react'
 
 interface Props{
-    word: String
+  word: string;
+  isSent: boolean;
 }
 
-const Result: React.FC<Props> = ({word}) => {
-
-  return (
-    <View style={tw`w-80 h-40 flex justify-center items-center border-4 border-gray-300 rounded-lg my-5`}>
-      <Text>{word}</Text>
+const Result: React.FC<Props> = ({word, isSent}) : any => {
+  const [definition, setDefinition] = useState(""); 
+  useEffect(() => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(url).then((response) => {
+      console.log(response.data)
+      setDefinition(response.data[0].meanings[0].definitions[0].definition)
+    })
+    .catch((err) => {
+      console.log(err)
+    });  
+    console.log(url)
+  }, [word])
+  return ( 
+    <View style={tw`w-80 h-40 flex justify-center items-center border-4 border-gray-300 rounded-lg my-5 p-2 ${isSent ? 'opacity-100' : 'opacity-0'}`}>
+      <Text>{definition}</Text>
     </View>
   )
 }
 
-export default Result
+export default Result;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -7,8 +7,12 @@ import Result from '../components/Result';
 
 const HomeScreen = () => {
   const [word, setWord] = useState("");
-  
-  console.log(word)
+  const [isSent, setIsSent] = useState(false);
+  console.log( word );
+  const onSubmitHandler = (event: any) => {
+    setIsSent(true);
+    setWord(event.nativeEvent.text)
+  }
   return (
     <ScrollView contentContainerStyle={tw`flex-col justify-center items-center`} style={tw`h-full py-20`}>
       <View style={tw`border-2 border-gray-300 h-10 w-80 px-2 rounded-full flex-row`}> 
@@ -16,11 +20,11 @@ const HomeScreen = () => {
             <Ionicons  name="search" size={20} color="gray"/>
         </View>
         <TextInput 
-        onSubmitEditing={(event) => setWord(event.nativeEvent.text)}
+        onSubmitEditing={(event) => onSubmitHandler(event)}
         placeholder="Please enter a word..." 
         underlineColorAndroid="transparent" />
       </View>
-      <Result word={word}/>
+      <Result word={word} isSent={isSent}/>
     </ScrollView>
   )
 }
